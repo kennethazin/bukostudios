cbecdaed99cd7c4b5f9fc5548cdcb637fc8a6e81
kennethazin <kenneth.ras3@mail.dcu.ie>
2024-08-15 22:12:33 +0100

    chore: Update work page with under construction message

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,7 @@
 export default function Work() {
   return (
     <section className="min-h-screen">
-      <div>test</div>
+      <h4 className="flex justify-center ">Under construction 🚧</h4>
     </section>
   );
 }
