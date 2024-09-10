48ad39bc8f375864b1a4332ce92c666c2b76642e
Kenneth Ras <65217445+kennethazin@users.noreply.github.com>
2024-09-10 18:48:32 +0100

    add inquiry to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -27,6 +27,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: "https://witespacestudios.com/inquiry",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: "https://witespacestudios.com/blog",
       lastModified: new Date(),
       changeFrequency: "weekly",
