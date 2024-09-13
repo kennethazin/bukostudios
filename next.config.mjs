2b577e2cc93163932df4a63393243408fe5aab05
Kenneth Ras <65217445+kennethazin@users.noreply.github.com>
2024-09-14 00:25:45 +0100

    fixing process env

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    RESEND_API_KEY: process.env.NEXT_PUBLIC_RESEND_API_KEY,
-    RESEND_FROM_EMAIL: process.env.NEXT_PUBLIC_RESEND_FROM_EMAIL,
-  },
-};
+const nextConfig = {};
 export default nextConfig;
