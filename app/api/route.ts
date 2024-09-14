5fedbb0ccc9c3aa9af4644fd2e624f13884dee76
Kenneth Ras <65217445+kennethazin@users.noreply.github.com>
2024-09-14 12:25:48 +0100

    add use server

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,3 +1,4 @@
+"use server"
 import { NextRequest, NextResponse } from 'next/server';
 import { Resend } from 'resend';
 import { EmailTemplate, EmailTemplateFull } from "@/components/email-template";
