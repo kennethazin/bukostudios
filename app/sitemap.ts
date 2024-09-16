05b5dc8e61c37bd4b0c81627653fec2325c1fb95
Kenneth Ras <65217445+kennethazin@users.noreply.github.com>
2024-09-16 10:18:17 +0100

    sitemap changr

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,30 +5,50 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://www.witespacestudios.com",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
-    {
+        {
       url: "https://www.witespacestudios.com/",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.witespacestudios.com/404",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
     {
       url: "https://www.witespacestudios.com/info",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
     {
       url: "https://www.witespacestudios.com/about",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
     {
       url: "https://www.witespacestudios.com/work",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
     {
       url: "https://www.witespacestudios.com/inquiry",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
     {
       url: "https://www.witespacestudios.com/blog",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
   ];
 }
