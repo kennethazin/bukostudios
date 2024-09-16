3504fa876ed806b2d740c8d3493a957ea83e5d0a
Kenneth Ras <65217445+kennethazin@users.noreply.github.com>
2024-09-16 13:42:32 +0100

    add navbar to notfound

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,16 @@
 "use client"
 import Link from "next/link"
 import Image from "next/image"
+import Navbar from '../components/Navbar';
 
 export default function NotFound() {
   return (
-    <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+    <>
+    <Navbar />
+    <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8 ">
       <div className="mx-auto max-w-md text-center flex flex-col justify-center items-center">
 
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-4xl">Oops, page not found!</h1>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-4xl transition-all duration-150 ease-in-out">Oops, page not found!</h1>
         <p className="mt-4 text-muted-foreground">
           The page you&apos;re looking for doesn&apos;t exist. Let&apos;s get you back on track.
         </p>
@@ -22,5 +25,6 @@ export default function NotFound() {
         </div>
       </div>
     </div>
+    </>
   )
 }
