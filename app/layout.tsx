72c392f33cd316d65181e0b2ee45ff5fd24bcba4
Kenneth Ras <65217445+kennethazin@users.noreply.github.com>
2024-09-29 15:53:01 +0100

    change default theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <body className="bg-background">
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
