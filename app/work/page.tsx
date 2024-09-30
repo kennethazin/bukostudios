fca67772efd6e5ade4e63c1642862c09b0a98615
kennethazin <kenneth.ras3@mail.dcu.ie>
2024-09-30 12:18:44 +0100

    fix flex on h1 tag for work

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,16 +2,17 @@ import Image from "next/image";
 
 export default function Work() {
   return (
-    <section className="min-h-screen flex justify-center items-center w-full">
-      <div className="flex flex-col gap-y-5">
-
+    <section className="min-h-screen justify-center items-center w-full">
+      <div className="flex flex-col gap-3 mb-5">
         <h1 className="uppercase tracking-tighter font-bold">East City</h1>
         <div className="border border-black w-fit border- py-1 px-3 rounded-3xl font-light text-base">
           Web design & development
         </div>
+      </div>
+      <div className="flex flex-col gap-y-5">
         <a href="https://eastcity.framer.website">
           <Image
-            src="/assets/eastcity/eastcity-ipad.png"
+            src="/assets/eastcity/ipad.webp"
             width={2000}
             height={4000}
             alt="East City iPad Mockup"
@@ -23,7 +24,7 @@ export default function Work() {
         </a>
         <a href="https://eastcity.framer.website">
           <Image
-            src="/assets/eastcity/eastcity-iphone.png"
+            src="/assets/eastcity/iphone.webp"
             width={5184}
             height={3456}
             alt="East City iPad Mockup"
@@ -32,7 +33,7 @@ export default function Work() {
         </a>
         <a href="https://eastcity.framer.website">
           <Image
-            src="/assets/eastcity/desktop.png"
+            src="/assets/eastcity/desktop.webp"
             width={5184}
             height={3456}
             alt="East City iPad Mockup"
