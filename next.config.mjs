c962bdf8097070479f9ed4f8d061a5647fadb20e
kennethazin <kenneth.ras3@mail.dcu.ie>
2024-09-30 12:48:00 +0100

    fix malloc error

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,7 @@ import withPlaiceholder from "@plaiceholder/next";
  * @type {import('next').NextConfig}
  */
 const config = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-  }
+  // your Next.js config
 };
  
 export default withPlaiceholder(config);
